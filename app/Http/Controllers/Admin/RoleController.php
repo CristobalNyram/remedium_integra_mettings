7a17d9ddd63bc5de8d1848539bc9e3d8a2466271
cristobal nyram <crisco.nyram@gmail.com>
2022-12-02 07:55:51 -0600

    base of the project

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('can:role_index')->only('index');
+    }
     public function index()
     {
         return view('admin.role.index');
